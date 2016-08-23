7b26da68f1489e9dfbe6976944b7841a58526e10
Eydamos <t.werdin@eydamos.de>
2016-08-23 16:20:23 +0200

    [BUGFIX] Fixed error preventing client to join server

--- src/main/java/de/eydamos/backpack/handler/HandlerCommonEvents.java
+++ src/main/java/de/eydamos/backpack/handler/HandlerCommonEvents.java
@@ -3,6 +3,7 @@ package de.eydamos.backpack.handler;
 import de.eydamos.backpack.misc.Constants;
 import net.minecraftforge.common.DimensionManager;
 import net.minecraftforge.event.world.WorldEvent;
+import net.minecraftforge.fml.common.FMLLog;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 import java.io.File;
@@ -10,16 +11,24 @@ import java.io.File;
 public class HandlerCommonEvents {
     @SubscribeEvent
     public void onWorldLoad(WorldEvent.Load event) {
-        File dataDir = new File(DimensionManager.getCurrentSaveRootDirectory(), "data");
+        try {
+            File dataDir = new File(DimensionManager.getCurrentSaveRootDirectory(), "data");
 
-        File backpackDir = new File(dataDir, Constants.INVENTORIES_PATH);
-        if (!backpackDir.exists()) {
-            backpackDir.mkdirs();
-        }
+            File backpackDir = new File(dataDir, Constants.INVENTORIES_PATH);
+            if (!backpackDir.exists()) {
+                if (!backpackDir.mkdirs()) {
+                    FMLLog.warning("Unable to create backpack/inventory folder. Saving backpack inventory will fail!");
+                }
+            }
 
-        File playersDir = new File(dataDir, Constants.PLAYERS_PATH);
-        if (!playersDir.exists()) {
-            playersDir.mkdirs();
+            File playersDir = new File(dataDir, Constants.PLAYERS_PATH);
+            if (!playersDir.exists()) {
+                if (!playersDir.mkdirs()) {
+                    FMLLog.warning("Unable to create backpack/player folder. Saving player specific backpack data will fail!");
+                }
+            }
+        } catch (Exception e) {
+            FMLLog.info("Unable to check if backpack folders are present. If you are a client connecting to a server ignore this info.");
         }
     }
 }

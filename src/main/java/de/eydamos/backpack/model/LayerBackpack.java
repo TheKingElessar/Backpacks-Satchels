b53751e2ae746a056943915485b022b02f151d11
Kenny Jackson <dsgamecube@gmail.com>
2019-10-02 02:33:39 +0000

    Fix NPE in doRenderLayer spamming console log and rendering game unplayable

--- src/main/java/de/eydamos/backpack/model/LayerBackpack.java
+++ src/main/java/de/eydamos/backpack/model/LayerBackpack.java
@@ -32,6 +32,7 @@ public class LayerBackpack implements LayerRenderer<AbstractClientPlayer> {
         float scale
     ) {
         ItemStack backpack = Backpack.proxy.getClientBackpack(player);
+        if(backpack == null) return;
 
         if (!backpack.isEmpty()) {
             GL11.glPushMatrix();

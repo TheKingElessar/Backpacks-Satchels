467857447b9c2156efa7ca2c07993d34b29ecf33
TheKingElessar <wasabimath@gmail.com>
2020-06-19 18:30:22 -0600

    Fixed main hand item being locked when the player is in their backpack slot backpack

--- src/main/java/de/eydamos/backpack/storage/slot/SlotMainHand.java
+++ src/main/java/de/eydamos/backpack/storage/slot/SlotMainHand.java
@@ -1,16 +1,21 @@
 package de.eydamos.backpack.storage.slot;
 
+import de.eydamos.backpack.helper.BackpackHelper;
 import de.eydamos.guiadvanced.inventory.SlotWithState;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.IInventory;
 
-public class SlotMainHand extends SlotWithState {
-    public SlotMainHand(IInventory inventoryIn, int index, int xPosition, int yPosition) {
+public class SlotMainHand extends SlotWithState
+{
+    public SlotMainHand(IInventory inventoryIn, int index, int xPosition, int yPosition)
+    {
         super(inventoryIn, index, xPosition, yPosition);
     }
-
+    
     @Override
-    public boolean canTakeStack(EntityPlayer playerIn) {
-        return false;
+    public boolean canTakeStack(EntityPlayer playerIn)
+    {
+        if (BackpackHelper.isBackpack(playerIn.getHeldItemMainhand())) return false;
+        else return true;
     }
 }
